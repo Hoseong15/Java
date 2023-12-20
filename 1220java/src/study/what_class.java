67382c4dd16622f260482a6c8f1c028c7ff48a00
Hoseong15 <dlsghtjd3982@gamil.com>
2023-12-20 09:59:29 +0900

    클래스

---
+++ 1220java/src/study/what_class.java
@@ -0,0 +1,13 @@
+package study;
+
+public class what_class {
+
+	public static void main(String[] args) {
+		// 클래스 란 - 사용자 정의 데이터 타입
+		// 기존의 기본 데이터 타입이 아닌 새로운 형태의 타입을
+		// 직접 정하는것,
+		// 기본 데이터타입과의 차이점은 여러종류의 데이터를 다룰수도 있으면
+		// 기능(매서드)을 정의 할 수 있다.
+	}
+
+}
