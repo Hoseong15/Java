8517698116f78fce29cc745194f5aed7f88e6eb2
Hoseong15 <dlsghtjd3982@gamil.com>
2023-12-28 12:51:07 +0900

    Java interface

---
+++ 1226java_inherit/src/java_interface/unit.java
@@ -0,0 +1,27 @@
+package java_interface;
+
+
+public interface unit {
+	
+	
+	public void move(); // 추상메서드
+	public void attack();
+	public void dead();
+	
+	
+// 추상클래스
+// abstract class score{
+// abstract void method();
+// }
+	
+// public final int run = 10; // final제어자가 불은 변수(상수)선언 가능
+// public static void output() { // 클래스메서드 정의가능
+//		
+// }
+// public void input() { // 인스턴스 메서드 정의불가}
+//	
+// static int num = 0; //클래스변수 선언 가능하지만 초기값지정 필수
+// int num; 인스턴스 변수 선언불가
+
+
+}
