66660ecf8613a9fac1ebc16352af3dbe9c9761cc
Hoseong15 <dlsghtjd3982@gamil.com>
2024-01-02 17:31:02 +0900

    Java ArrayList

---
+++ java_collection/src/arraylist/box.java
@@ -0,0 +1,11 @@
+package arraylist;
+
+public class box<T> {
+
+	
+	public void createBox(T o ) {
+		System.out.println(o);	
+	}
+	
+
+}
--- java_collection/src/arraylist/java_study.java
+++ java_collection/src/arraylist/java_study.java
@@ -1,23 +1,80 @@
 package arraylist;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class java_study {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Integer> arr = new ArrayList<Integer>();
+		ArrayList<member> mbr = new ArrayList<>(); // 앞에 작성을 하면 뒤에있는 <>안에 안해도된다.
 		
+		mbr.add(new member("이순신", "lee@gmail.com", "남성", 1012345678));
+		mbr.add(new member("김유신", "kim@naver.com", "남성", 1023456789));
+		mbr.add(new member("장영실", "jang@naver.com", "남성", 1015678234));
+		mbr.add(new member("소서노", "so@naver.com", "여성", 1022769298));
+		mbr.add(new member("신사임당", "shin@naver.com", "여성", 1022769298));
 		
-		arr.add(10);
-		arr.add(20);
-		arr.add(30);
-//		arr.add("이순신");
+		System.out.println(mbr.get(2));
+		System.out.println(mbr.size());
+		System.out.println(mbr.subList(1, 4));
+		
+		// indexOf()
+		member tmp = new member("소서노", "so@naver.com", "여성", 1022769298);
+		System.out.println(mbr.indexOf(tmp));
+		System.out.println(mbr.contains("김유신"));
 
-		for(int i = 0; i < arr.size(); i++) {
-			if(arr.get(i) > 20)
-				System.out.println(arr.get(i));
-		}
+		
+		
+		
+		
+		
+		
+//		ArrayList<Integer> arr = new ArrayList<Integer>();
+//		
+//		
+//		arr.add(10);
+//		arr.add(20);
+//		arr.add(30); // 배열의 마지막에 새로운 데이터 추가
+//		arr.add(1,100); // 배열의 지정한 인덱스 위치에 새로운 데이터 추가
+////		arr.add("이순신");
+//
+//		for(int i = 0; i < arr.size(); i++) {
+//			if(arr.get(i) > 20)
+//				System.out.println(arr.get(i));
+//		}
+//		
+//		System.out.println("arraylist에 값 존재여부 contanins : " + arr.contains(20));
+//		System.out.println("값이 몇번째 인덱스 indexOf : "+arr.indexOf(30));
+//		System.out.println("arraylist에 값이 몇개 있냐 size : "+arr.size());
+//		arr.remove(2); // 지정한 인덱스 위치의 값 삭제
+//		System.out.println("arraylist 비어있나 isEmpty : " + arr.isEmpty());
+//		System.out.println("범위내 값 출력 subList : " + arr.subList(1, 3));
+//		
+//		arr.set(1, 200);
+//		System.out.println("갑 변경 set : " + arr);
+//		
+//		//arr.toArray() - ArrayList를 정적 배열로 변환
+//		
+//		ArrayList<String> name = new ArrayList<String>();
+//		name.add("이순신"); name.add("강감찬"); name.add("김유신"); name.add("장영실");
+//		String[] names = new String[name.size()];
+//		name.toArray(names);
+//		System.out.println(Arrays.toString(names));
+//		
+//		//정렬
+////		Collections.sort(arr); // 사전적 슌서 정렬
+//		arr.sort(Comparator.naturalOrder()); // 사전적 순서 정렬
+//		arr.sort(Comparator.reverseOrder()); // 사전적 순서의 역방향 정렬
+//		System.out.println(arr);
+//		
+//		Collections.sort(name);
+//		System.out.println(name);
+		
+		
+		
 	}
 
 }
