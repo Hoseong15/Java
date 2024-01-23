290e174de9b49b2f4d57869e128f515589cefa02
Hoseong15 <dlsghtjd3982@gamil.com>
2024-01-23 17:32:04 +0900

    Java model2 login

---
+++ model2/src/main/webapp/static/js/member/signup.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+ $(function() {
+	
+	
+	$('.input_data input').on("keyup", () => {
+		var i = 0;
+		for( ; i < $('.input_data input').length; i++) {
+			if( $('.input_data input').eq(i).val() == '')
+			break;
+		}
+		if(i == $('.input_data input').length){
+			$('#bt').attr("disabled",false);
+			$('#bt').removeClass("inactive");
+		}else {
+			$('#bt').attr("disabled",true);
+			$('#bt').addClass("inactive");
+		}
+		
+	});
+	
+	$("#bt").on("click",() => { // 회원가입 버튼 클릭시
+		if($("#pin").val() != $("#pin2").val()) {
+			alert("비밀번호를 확인하세요");
+			$("#pin").val("");
+			$("#pin2").val("");
+			$("#pin").focus();
+			$("#pin2").focus();
+			$('#bt').attr("disabled",true);
+			$('#bt').addClass("inactive");
+		}else{
+			$("#signupFm").submit(); // form내부의 데이터 서버에 전달
+		}
+	});
+	
+	
+});
+ 
+ 
